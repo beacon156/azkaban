c63f681404feb4d33a833886f551ea75e7b98e6e
poem <xue_2013@sina.com>
2020-07-27 00:15:03 +0800

    修改地址

--- src/main/java/org/poem/AzkabanDataPushJobApplication.java
+++ src/main/java/org/poem/AzkabanDataPushJobApplication.java
@@ -1,12 +1,18 @@
 package org.poem;
 
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AzkabanDataPushJobApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(AzkabanDataPushJobApplication.class);
+
     public static void main(String[] args) {
+        logger.info("pars:\n {}", JSONObject.toJSONString(args));
         SpringApplication.run(AzkabanDataPushJobApplication.class, args);
     }
 
